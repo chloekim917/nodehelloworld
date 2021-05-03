29ff3185cf23c7f5a9efb90335dd67816fc2a68c
chloekim917 <chloekim917@gmail.com>
2021-05-03 18:35:46 -0400

    node practice

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
+// var http = require('http');
+// var dt = require('./myfirstmodule');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080);
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write("The date and time are currently: " + dt.myDateTime());
+//   res.end();
+// }).listen(8080);
